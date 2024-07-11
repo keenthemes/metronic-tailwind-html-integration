87accbeb15bf37a39a3685f1fd1ad7f3b0878da1
faizal <jai.crys@gmail.com>
2024-07-11 08:48:44 +0800

    angular layout update

--- metronic-tailwind-angular/src/app/layouts/sidebar/sidebar.component.ts
+++ metronic-tailwind-angular/src/app/layouts/sidebar/sidebar.component.ts
@@ -8,7 +8,7 @@ import { Component, HostBinding } from '@angular/core';
 	styleUrl: './sidebar.component.scss'
 })
 export class SidebarComponent {
-	@HostBinding('class') hostClass = 'sidebar dark:bg-coal-600 bg-light border-r border-r-gray-200 dark:border-r-coal-100 fixed top-0 bottom-0 z-20 hidden lg:flex flex-col items-stretch shrink-0';
+	@HostBinding('class') hostClass = 'sidebar dark:bg-coal-600 bg-light border-r border-r-gray-200 dark:border-r-coal-100 fixed z-20 hidden lg:flex flex-col items-stretch shrink-0';
 	@HostBinding('attr.data-drawer') drawer = 'true';
 	@HostBinding('attr.data-drawer-class') drawerClass = 'drawer drawer-start top-0 bottom-0';
 	@HostBinding('attr.data-drawer-enable') drawerEnable = 'true|lg:false';
