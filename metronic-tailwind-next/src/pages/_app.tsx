45b180e53a28ea0acc35f2cddcccdd495ad43b18
laurisstepanovs <33573735+laurisstepanovs@users.noreply.github.com>
2024-07-16 00:50:10 +0300

    Add layout components and core ts plugins initialization

--- metronic-tailwind-next/src/pages/_app.tsx
+++ metronic-tailwind-next/src/pages/_app.tsx
@@ -1,6 +1,40 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import Footer from "@/components/Footer";
+import Sidebar from "@/components/Sidebar";
+import Header from "@/components/Header";
+import SearchModal from "@/components/SearchModal";
+import {useEffect} from "react";
+import KTComponent from "@/metronic/core";
+import KTLayout from "@/metronic/app/layouts/demo1";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  useEffect(() => {
+    require("../metronic/vendors/keenicons/outline/style.css");
+    require("../metronic/vendors/keenicons/solid/style.css");
+    require("../metronic/vendors/keenicons/filled/style.css");
+
+    if (typeof window !== 'undefined') {
+      KTComponent.init();
+      KTLayout.init();
+    }
+  }, []);
+
+  return <>
+      <div className="flex grow">
+        <Sidebar />
+        <div className="wrapper flex grow flex-col">
+          <Header />
+          <main className="grow content pt-5" id="content" role="content">
+          <div className="container-fixed" id="content_container">
+          </div>
+          <div className="container-fixed">
+            <Component {...pageProps} />
+          </div>
+          </main>
+          <Footer />
+        </div>
+      </div>
+      <SearchModal />
+    </>
 }
--- metronic-tailwind-next/src/pages/_document.tsx
+++ metronic-tailwind-next/src/pages/_document.tsx
@@ -2,9 +2,9 @@ import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html lang="en">
+    <Html className="h-full light" data-theme="true" lang="en">
       <Head />
-      <body>
+      <body className="flex h-full demo1 sidebar-fixed header-fixed bg-[#fefefe] dark:bg-coal-500">
         <Main />
         <NextScript />
       </body>
