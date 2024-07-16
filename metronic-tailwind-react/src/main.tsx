4c6d329e05457ec10a17c3c794a3c115570f8739
laurisstepanovs <33573735+laurisstepanovs@users.noreply.github.com>
2024-07-16 23:32:47 +0300

    Remove strict mode wrapper

--- metronic-tailwind-react/src/main.tsx
+++ metronic-tailwind-react/src/main.tsx
@@ -3,8 +3,4 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById('root')!).render(<App />)
