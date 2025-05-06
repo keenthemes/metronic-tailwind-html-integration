3746cab1f459b8804662d50b324b9ed113f9c71e
faizal <jai.crys@gmail.com>
2025-05-06 15:25:25 +0800

    Clean install

--- metronic-tailwind-laravel/resources/js/hooks/use-mobile-navigation.ts
+++
@@ -1,8 +0,0 @@
-import { useCallback } from 'react';
-
-export function useMobileNavigation() {
-    return useCallback(() => {
-        // Remove pointer-events style from body...
-        document.body.style.removeProperty('pointer-events');
-    }, []);
-}
--- metronic-tailwind-laravel/routes/web.php
+++ metronic-tailwind-laravel/routes/web.php
@@ -1,17 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use Inertia\Inertia;
 
 Route::get('/', function () {
-    return Inertia::render('welcome');
-})->name('home');
-
-Route::middleware(['auth', 'verified'])->group(function () {
-    Route::get('dashboard', function () {
-        return Inertia::render('dashboard');
-    })->name('dashboard');
+    return view('welcome');
 });
-
-require __DIR__.'/settings.php';
-require __DIR__.'/auth.php';
