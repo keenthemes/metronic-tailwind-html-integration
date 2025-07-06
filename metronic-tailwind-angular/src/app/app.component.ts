f1ed0a445b1f44a8b4e9b0707eb76a76ace8e0a1
faizal <jai.crys@gmail.com>
2025-07-07 07:51:39 +0800

    chore: update project configuration and dependencies, including the addition of PostCSS and Tailwind CSS support, removal of unused components, and adjustments to Angular settings for improved performance and maintainability.

--- metronic-tailwind-angular/src/app/app.component.ts
+++ metronic-tailwind-angular/src/app/app.component.ts
@@ -1,24 +1,12 @@
-import { AfterViewInit, Component, HostBinding, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { HeaderComponent } from './layouts/header/header.component';
-import { FooterComponent } from './layouts/footer/footer.component';
-import { SidebarComponent } from './layouts/sidebar/sidebar.component';
-import { SearchModalComponent } from './partials/search-modal/search-modal.component';
 
 @Component({
-    selector: 'app-root',
-    imports: [RouterOutlet, HeaderComponent, FooterComponent, SidebarComponent, SearchModalComponent],
-    templateUrl: './app.component.html',
-    styleUrl: './app.component.scss'
+  selector: 'app-root',
+  imports: [RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
 })
-export class AppComponent implements AfterViewInit, OnInit {
-	title = 'metronic-tailwind-angular';
-	@HostBinding('class') hostClass = 'flex grow';
-
-	ngAfterViewInit(): void {
-	}
-
-	ngOnInit(): void {
-	}
+export class AppComponent {
+  title = 'metronic-tailwind-angular';
 }
-
--- metronic-tailwind-angular/src/app/app.routes.ts
+++ metronic-tailwind-angular/src/app/app.routes.ts
@@ -1,7 +1,3 @@
 import { Routes } from '@angular/router';
-import { IndexComponent } from './pages/index/index.component';
 
-export const routes: Routes = [
-	{ path: '', component: IndexComponent, pathMatch: 'full' },
-];
-
+export const routes: Routes = [];
